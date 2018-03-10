f471da998a0ec98e940c504e04112892206dd7bd
Yousef Sultan <yousef.su.2000@gmail.com>
2018-03-11 01:00:53 +0200

    Move snippets deck to separate component
    Signed-off-by: Yousef Sultan <yousef.su.2000@gmail.com>

---
+++ assets/js/components/SnippetsDeck.vue
@@ -0,0 +1,61 @@
+<template>
+    <div id="snippets-container" class="row">
+        <div class="col-xs-12 col-sm-6 col-md-4" v-for="snippet in snippets" :key="snippet.id">
+            <router-link :to="{name: 'view-snippet', params: {id: snippet.id}}" class="nostyle">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">{{snippet.title}}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">
+                            <span class="fa fa-star"></span> {{snippet.votes}}
+                            <span class="fa fa-code"></span> {{snippet.language.name}}
+                            <span class="fa fa-eye"></span> {{snippet.views}}
+                            <span class="fa fa-clock-o"></span> {{moment(snippet.updatedAt).fromNow()}}
+                        </h6>
+                        <div class="card-text">
+                            {{snippet.description ? shorten(summarize(snippet.description), 200) : 'No description provided.'}}
+                        </div>
+                    </div>
+                </div>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script type="text/javascript">
+    import {shorten} from '../helpers';
+    import summarize from 'summarize-markdown';
+
+    export default {
+        props: {
+            snippets: {
+                type: Array,
+                required: true
+            }
+        },
+
+        methods: {
+            moment: moment.utc,
+            shorten, summarize
+        }
+    }
+</script>
+
+<style scoped>
+	#snippets-container {
+		margin-top: 24px;
+	}
+
+	#snippets-container .card {
+		height: 280px;
+		margin-bottom: 48px;
+		overflow: hidden;
+	}
+
+	.card-subtitle span {
+		margin-left: 8px;
+	}
+
+	.card-subtitle span:first-child {
+		margin-left: 0;
+	}
+</style>
--- assets/js/components/pages/Discover.vue
+++ assets/js/components/pages/Discover.vue
@@ -8,34 +8,14 @@
 			</div>
 			<hr>
 
-			<div id="snippets-container" class="row">
-				<div class="col-xs-12 col-sm-6 col-md-4" v-for="snippet in snippets" :key="snippet.id">
-					<router-link :to="{name: 'view-snippet', params: {id: snippet.id}}" class="nostyle">
-						<div class="card">
-							<div class="card-body">
-								<h5 class="card-title">{{snippet.title}}</h5>
-								<h6 class="card-subtitle mb-2 text-muted">
-									<span class="fa fa-star"></span> {{snippet.votes}}
-									<span class="fa fa-code"></span> {{snippet.language.name}}
-									<span class="fa fa-eye"></span> {{snippet.views}}
-									<span class="fa fa-clock-o"></span> {{moment(snippet.updatedAt).fromNow()}}
-								</h6>
-								<div class="card-text">
-									{{snippet.description ? shorten(summarize(snippet.description), 200) : 'No description provided.'}}
-								</div>
-							</div>
-						</div>
-					</router-link>
-				</div>
-			</div>
+			<snippets-deck :snippets="snippets" v-if="snippets"></snippets-deck>
 		</div>
 	</div>
 </template>
 
 <script type="text/javascript">
-	import {shorten} from '../../helpers';
 	import {mapState} from 'vuex';
-	import summarize from 'summarize-markdown';
+	import SnippetsDeck from '../SnippetsDeck';
 
 	export default {
 		asyncData: function(store, route) {
@@ -53,53 +33,6 @@
 			snippets: state => state.snippets.new,
 		}),
 
-		mounted: function() {
-			moment.updateLocale('en', {
-				relativeTime: {
-					future: 'in %s',
-					past: '%s',
-					s:  'seconds',
-					ss: '%ss',
-					m:  'a minute',
-					mm: '%dm',
-					h:  'an hour',
-					hh: '%dh',
-					d:  'a day',
-					dd: '%dd',
-					M:  'a month',
-					MM: '%dM',
-					y:  'a year',
-					yy: '%dY'
-				}
-			});
-		},
-
-		destroyed: function() {
-			moment.updateLocale('en', {
-				relativeTime: {
-					future: 'in %s',
-					past: '%s ago',
-					s:  'a few seconds',
-					ss: '%s seconds',
-					m:  'a minute',
-					mm: '%d minutes',
-					h:  'an hour',
-					hh: '%d hours',
-					d:  'a day',
-					dd: '%d days',
-					M:  'a month',
-					MM: '%d months',
-					y:  'a year',
-					yy: '%d years'
-				}
-			});
-		},
-
-		methods: {
-			moment: moment.utc,
-			shorten, summarize,
-		},
-
 		meta: {
 			title:'Discover the awesome and the new',
 
@@ -109,29 +42,14 @@
 				{property: 'og:description', content: 'Discover new and cool snippets made by developers just like you, from around the entire world.'},
 			],
 		},
+
+		components: {
+			'snippets-deck': SnippetsDeck,
+		}
 	};
 </script>
 
 <style type="text/css" scoped>
-	#snippets-container {
-		margin-top: 24px;
-		width: 100vw;
-	}
-
-	#snippets-container .card {
-		height: 280px;
-		margin-bottom: 48px;
-		overflow: hidden;
-	}
-
-	.card-subtitle span {
-		margin-left: 8px;
-	}
-
-	.card-subtitle span:first-child {
-		margin-left: 0;
-	}
-
 	.green-text {
 		color: #4caf50;
 	}
