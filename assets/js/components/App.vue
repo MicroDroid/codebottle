ae514abd08d455d7864b79ac32da9c5638a97e38
Yousef Sultan <yousef.su.2000@gmail.com>
2018-08-05 10:11:12 +0300

    Open footer links in new tab
    Signed-off-by: Yousef Sultan <yousef.su.2000@gmail.com>

--- assets/js/components/App.vue
+++ assets/js/components/App.vue
@@ -9,10 +9,10 @@
 			<span>
 				<router-link :to="{name: 'api.getting-started'}">API</router-link>
 				| <router-link :to="{name: 'embedding'}">Embedding</router-link>
-				| <a href="https://discord.gg/2Sd8Um3" id="discord-invite">Discord</a>
+				| <a href="https://discord.gg/2Sd8Um3" target="_blank" id="discord-invite">Discord</a>
 				| <router-link :to="{name: 'privacy-policy'}">Privacy Policy</router-link>
-				| Built with <span class="fas fa-heart" title="Oh you burnt it!" /> by <a href="https://microdroid.github.io/">OverCoder</a>
-				| Proudly hosted by <a href="https://blackfields.net/">BFNT <span class="fal fa-globe" title="Globe!" /></a>
+				| Built with <span class="fas fa-heart" title="Oh you burnt it!" /> by <a href="https://microdroid.github.io/" target="_blank">OverCoder</a>
+				| Proudly hosted by <a href="https://blackfields.net/" target="_blank">BFNT <span class="fal fa-globe" title="Globe!" /></a>
 			</span>
 		</footer>
 	</div>
