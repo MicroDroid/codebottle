e53a0d8703af59316e15cbdb431221318b9f2bc0
Yousef Sultan <yousef.su.2000@gmail.com>
2018-09-21 17:05:30 +0300

    Fix mixed HTTP/HTTPS content
    Signed-off-by: Yousef Sultan <yousef.su.2000@gmail.com>

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const assets = require('./assets');
 const apiPort = process.env.API_PORT || 3000;
 const assetsPort = process.env.ASSETS_PORT || 3001;
 
+api.proxy = assets.proxy = true;
+
 api.listen(apiPort);
 logger.info(`API listening on port ${apiPort}`);
 
