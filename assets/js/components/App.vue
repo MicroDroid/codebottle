1ac2c735ded47cebe98b8fc83218f3eeab442180
Yousef Sultan <yousef.su.2000@gmail.com>
2019-10-03 17:58:01 +0300

    Update footer
    Signed-off-by: Yousef Sultan <yousef.su.2000@gmail.com>

--- assets/js/components/App.vue
+++ assets/js/components/App.vue
@@ -5,23 +5,14 @@
 			<navbar />
 			<router-view />
 		</div>
-		<footer>
-			<span>
-				<router-link :to="{name: 'api.getting-started'}">API</router-link>
-				| <router-link :to="{name: 'support'}" class="support-link">Support</router-link>
-				| <router-link :to="{name: 'embedding'}">Embedding</router-link>
-				| <a href="https://discord.gg/2Sd8Um3" target="_blank" class="discord-invite">Discord</a>
-				| <router-link :to="{name: 'privacy-policy'}">Privacy Policy</router-link>
-				| Built with <span class="fas fa-heart" title="Oh you burnt it!" /> by <a href="https://microdroid.github.io/" target="_blank">OverCoder</a>
-				| Proudly hosted by <a href="https://blackfields.net/" target="_blank">BFNT <span class="fal fa-globe" title="Globe!" /></a>
-			</span>
-		</footer>
+		<custom-footer />
 	</div>
 </template>
 
 <script type="text/javascript">
 	import Toast from './Toast';
-	
+	import Footer from './Footer';
+
 	export default {
 		meta: {
 			title: '404',
@@ -33,6 +24,7 @@
 
 		components: {
 			Toast,
+			'custom-footer': Footer,
 		},
 	};
 </script>
---
+++ assets/js/components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+	<footer>
+		<span>
+			<router-link :to="{name: 'api.getting-started'}">API</router-link>
+			| <router-link :to="{name: 'support'}" class="support-link">Support</router-link>
+			| <router-link :to="{name: 'embedding'}">Embedding</router-link>
+			| <a href="https://discord.gg/2Sd8Um3" target="_blank" class="discord-invite">Discord</a>
+			| <router-link :to="{name: 'privacy-policy'}">Privacy Policy</router-link>
+			| Built with <span class="fas fa-heart" title="Oh you burnt it!" /> by <a href="https://overcoder.dev/" target="_blank">OverCoder</a>
+			| Made possible with <a href="https://twitter.com/Xenthys" target="_blank">Xenthys</a>
+		</span>
+	</footer>
+</template>
+
+<style lang="scss" scoped>
+	footer {
+		padding: 1.25rem;
+		text-align: center;
+		color: #A0A0A7;
+		font-size: 12px;
+
+		.fa-heart {
+			color: #E91E63;
+		}
+
+		.discord-invite {
+			color: #7187d2;
+		}
+
+		.support-link {
+			color: #ffeb3b;
+		}
+
+		a, a:hover, a:focus, a:visited {
+			color: #A0A0A7;
+		}
+	}
+</style>
