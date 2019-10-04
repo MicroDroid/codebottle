b5df2d5558cb8c448274836bdab93eea1955d6fe
Yousef Sultan <yousef.su.2000@gmail.com>
2019-10-04 17:27:32 +0300

    Add Discord widget
    Signed-off-by: Yousef Sultan <yousef.su.2000@gmail.com>

--- assets/js/components/App.vue
+++ assets/js/components/App.vue
@@ -1,10 +1,19 @@
 <template>
 	<div id="app">
+		<iframe src="https://canary.discordapp.com/widget?id=222487241321086976&theme=dark"
+			width="320"
+			height="480"
+			allowtransparency="true"
+			frameborder="0"
+			class="discord-embed d-none d-lg-block"
+		/>
+
 		<div id="main-content">
 			<toast />
 			<navbar />
 			<router-view />
 		</div>
+
 		<custom-footer />
 	</div>
 </template>
@@ -28,3 +37,17 @@
 		},
 	};
 </script>
+
+<style lang="scss" scoped>
+	.discord-embed {
+		position: fixed;
+		right: 7.5vw;
+		bottom: -406px;
+
+		transition: bottom 0.3s ease;
+
+		&:hover {
+			bottom: 0px;
+		}
+	}
+</style>
