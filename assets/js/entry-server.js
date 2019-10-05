6a57ed9eaf73646b561e673d78eda7b9453b0c31
Yousef Sultan <yousef.su.2000@gmail.com>
2019-10-05 08:55:19 +0300

    Parallelize API requests during SSR
    Signed-off-by: Yousef Sultan <yousef.su.2000@gmail.com>

--- assets/js/entry-server.js
+++ assets/js/entry-server.js
@@ -14,8 +14,11 @@ export default context => {
 	return new Promise(async (resolve, reject) => {
 		const {app, router, store} = createApp();
 
-		await store.dispatch('fetchLanguages');
-		await store.dispatch('fetchCategories');
+		// Used to parallelize requests
+		const requests = [];
+
+		requests.push(store.dispatch('fetchLanguages'));
+		requests.push(store.dispatch('fetchCategories'));
 
 		if (context.authCookie) {
 			try {
@@ -24,8 +27,8 @@ export default context => {
 					root.document = {cookie: `auth=${context.authCookie}`};
 					store.commit('auth/LOGIN', auth);
 					root.axios.defaults.headers.common['Authorization'] = `Bearer ${auth.token}`;
-					await store.dispatch('auth/fetchPreferences');
-					await store.dispatch('users/fetchSelf');
+					requests.push(store.dispatch('auth/fetchPreferences'));
+					requests.push(store.dispatch('users/fetchSelf'));
 				}
 			} catch (e) {
 				console.log('Invalid auth cookie passed to SSR');
@@ -44,12 +47,17 @@ export default context => {
 
 		router.onReady(() => {
 			const matchedComponents = router.getMatchedComponents();
+
 			if (!matchedComponents.length)
 				return reject({ code: 404 });
-			Promise.all(matchedComponents.map(({ asyncData }) => asyncData && asyncData(
-				store,
-				router.currentRoute
-			))).then(() => {
+
+			Promise.all([
+				...requests,
+				...matchedComponents.map(({ asyncData }) => asyncData && asyncData(
+					store,
+					router.currentRoute
+				))
+			]).then(() => {
 				context.state = store.state;
 				resolve(app);
 			}).catch(reject);
