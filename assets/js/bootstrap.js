35b0859fccc8122d60ac6bb509eef27cb33ef596
Yousef Sultan <yousef.su.2000@gmail.com>
2020-05-21 17:25:20 +0300

    Add syntax highlighting for Swift and other languages
    Signed-off-by: Yousef Sultan <yousef.su.2000@gmail.com>

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -5,7 +5,7 @@ import moment from 'moment-mini';
 import axios from 'axios';
 
 // Will be replaced by server entry later, just to prevent undef errors
-if (typeof(window) === 'undefined')
+if (typeof (window) === 'undefined')
 	root.location = {
 		protocol: 'https:',
 		hostname: 'codebottle.io'
@@ -13,7 +13,10 @@ if (typeof(window) === 'undefined')
 
 root.hljs = hljs;
 
-const hljsLanguages = ['java', 'cpp', 'cs', 'python', 'php', 'javascript', 'perl', 'ruby', 'powershell', 'lua', 'json', 'crystal', 'dart'];
+const hljsLanguages = ['java', 'cpp', 'cs', 'css', 'python', 'php', 'javascript',
+	'perl', 'ruby', 'powershell', 'lua', 'json', 'bash', 'less', 'markdown', 'scss',
+	'sql', 'html', 'xml', 'yaml', 'crystal', 'dart', 'swift',
+];
 
 hljsLanguages.forEach(langName => {
 	const langModule = require(`highlight.js/lib/languages/${langName}`);
